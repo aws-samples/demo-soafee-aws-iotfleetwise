0b191c5fd088f9c0f2fd5bebd49bba58b59cec6e
Francesco Salamida <salamida.francesco@gmail.com>
2022-10-19 10:24:19 +0000

    first import

---
+++ deploy.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+echo -n "Cleaning up..."
+docker kill $(docker ps -q) > /dev/null 2>&1
+docker rm $(docker ps -aq) > /dev/null 2>&1
+ip link delete vxcan0 > /dev/null 2>&1
+echo "done"
+
+echo -n "Building and starting fwe container..."
+pushd fwe
+docker build . -t fwe
+popd
+docker run -d \
+       -e CAN_IF=vxcan1 \
+       -e FW_ENDPOINT=a1q6dgk6qorfqj-ats.iot.eu-central-1.amazonaws.com \
+       -e VEHICLE_NAME=vin100 \
+       --mount type=bind,source=$(pwd)/private-key.key,target=/etc/aws-iot-fleetwise/private-key.key,readonly \
+       --mount type=bind,source=$(pwd)/certificate.pem,target=/etc/aws-iot-fleetwise/certificate.pem,readonly \
+       -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+       --tmpfs /tmp \
+       --tmpfs /run \
+       --tmpfs /run/lock \
+       --name fwe \
+       fwe  
+echo "done"
+
+echo -n "Starting vsim container..."
+pushd vsim
+docker build . -t vsim
+popd
+docker run -d \
+       --name vsim \
+       vsim
+echo "done"
+  
+echo -n "Bringing up CAN bus..."
+DOCKERPID_FWE=$(docker inspect -f '{{ .State.Pid }}' fwe)
+DOCKERPID_VSIM=$(docker inspect -f '{{ .State.Pid }}' vsim)
+ip link add vxcan0 type vxcan peer name vxcan1
+ip link set vxcan0 netns $DOCKERPID_VSIM
+ip link set vxcan1 netns $DOCKERPID_FWE
+nsenter -t $DOCKERPID_VSIM -n ip link set vxcan0 up
+nsenter -t $DOCKERPID_FWE -n ip link set vxcan1 up
+echo "done"
+
+echo "Press CTRL+C to exit"
+echo "---"
+docker logs -f fwe
---
+++ fwe/start-fwe.sh
@@ -0,0 +1,28 @@
+function ifup {
+    typeset output
+    output=$(ip link show "$1" up) && [[ -n $output ]]
+}
+
+while : ; do
+    if ifup $CAN_IF; then
+        break
+    else
+        echo "Waiting for $CAN_IF"
+        sleep 3
+    fi
+done
+
+mkdir -p /var/aws-iot-fleetwise
+mkdir -p /etc/fwe
+cd /root/aws-iot-fleetwise-edge
+./tools/configure-fwe.sh \
+        --input-config-file configuration/static-config.json \
+        --output-config-file /etc/aws-iot-fleetwise/config-0.json \
+        --vehicle-name $VEHICLE_NAME \
+        --endpoint-url $FW_ENDPOINT \
+        --can-bus0 $CAN_IF 
+#sed -i 's/Info/Trace/' /etc/aws-iot-fleetwise/config-0.json 
+cat /etc/aws-iot-fleetwise/config-0.json
+/usr/bin/aws-iot-fleetwise-edge /etc/aws-iot-fleetwise/config-0.json
+/usr/bin/candump $CAN_IF
+
