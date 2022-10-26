4b6a4a119f6d34a015d1dcd6a440df43c4792d57
Francesco Salamida <salamida.francesco@gmail.com>
2022-10-26 15:08:15 +0000

    updated k3s

--- deploy-k3s.sh
+++ deploy-k3s.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+
+CAN_IF=vcan0
+FW_ENDPOINT="a1q6dgk6qorfqj-ats.iot.eu-central-1.amazonaws.com"
+VEHICLE_NAME=vin200
+TRACE=off
+
+# Clean up
+kubectl delete pod fwe
+kubectl delete svc vsim-svc
+kubectl delete ingress bcw-demo
+
+# Make sure secrets are there for key and cert
+#
+# kubectl create secret generic private-key --from-file=./private-key.key
+# kubectl create secret generic certificate --from-file=./certificate.pem
+
+# Deploy
+kubectl apply -f - <<EOF
 apiVersion: v1
 kind: Pod
 metadata:
@@ -10,13 +29,13 @@ spec:
     image: ghcr.io/fsalamida/aws-iot-fleetwise-edge:feature-publish_container_image  
     env:
     - name: CAN_IF
-      value: "vcan0"
+      value: "$CAN_IF"
     - name: FW_ENDPOINT
-      value: "a1q6dgk6qorfqj-ats.iot.eu-central-1.amazonaws.com"
+      value: "$FW_ENDPOINT"
     - name: VEHICLE_NAME
-      value: "vin200"
+      value: "$VEHICLE_NAME"
     - name: TRACE
-      value: "off"
+      value: "$TRACE"
     volumeMounts:
     - name: private-key
       mountPath: "/etc/aws-iot-fleetwise/private-key.key"
@@ -58,10 +77,29 @@ metadata:
 spec:
   selector:
     app: bcw-demo
-  type: NodePort
   ports:
   - protocol: TCP
     port: 3000
-    targetPort: 3000 
-    nodePort: 30000
+---
+kind: Ingress
+apiVersion: networking.k8s.io/v1
+metadata:
+  name: bcw-demo
+  annotations:
+    traefik.ingress.kubernetes.io/router.entrypoints: web
+spec:
+  rules:
+    - http:
+        paths:
+          - path: /
+            pathType: Prefix
+            backend:
+              service:
+                name: vsim-svc
+                port:
+                  number: 3000
+EOF
+kubectl wait --for=condition=ready pod -l app=bcw-demo
 
+# Show log from fwe
+kubectl logs -f fwe -c fwe
