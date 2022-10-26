51c5cb4d3522a492f66789ca1be4cee6be551abf
Francesco Salamida <salamida.francesco@gmail.com>
2022-10-26 14:22:47 +0000

    wip

--- deploy-k8s.sh
+++ deploy-k8s.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 kubectl delete pod fwe
+kubectl delete svc vsim-svc
 kubectl apply -f deploy.yml
 kubectl wait --for=condition=ready pod -l app=bcw-demo
 
@@ -7,8 +8,8 @@ echo -n "Bringing up CAN bus..."
 CRICTL="crictl -r unix:///run/k3s/containerd/containerd.sock"
 PODID=$($CRICTL pods --name fwe -o json | jq -r '.items[0].id')
 PID_FWE=$($CRICTL inspectp $PODID | jq -r '.info.pid')
-nsenter -t $PID_FWE -n ip link add dev vcan0 type vcan
-nsenter -t $PID_FWE -n ip link set vcan0 up
+#nsenter -t $PID_FWE -n ip link add dev vcan0 type vcan
+#nsenter -t $PID_FWE -n ip link set vcan0 up
 echo "done"
 
 kubectl logs -f fwe -c fwe
--- vsim/api/start-api.sh
+++ vsim/api/start-api.sh
@@ -3,6 +3,9 @@ function ifup {
     output=$(ip link show "$1" up) && [[ -n $output ]]
 }
 
+ip link add dev $CAN_IF type vcan
+ip link set $CAN_IF up
+
 while : ; do
     if ifup $CAN_IF; then
         break
