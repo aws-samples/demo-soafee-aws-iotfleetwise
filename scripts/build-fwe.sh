01e73ca889df907e0da985ec9c5373bab43d8020
Francesco Salamida <salamida.francesco@gmail.com>
2022-11-13 08:46:42 +0000

    fix file test

--- scripts/build-fwe.sh
+++ scripts/build-fwe.sh
@@ -2,7 +2,7 @@
 
 pushd fwe
 tar -czh . | docker build -t fwe -
-if [ -f /run/k3s/containerd/containerd.sock ]; then
+if [ -e /run/k3s/containerd/containerd.sock ]; then
 docker save fwe:latest | ctr -a /run/k3s/containerd/containerd.sock -n=k8s.io images import -
 fi
 popd
--- scripts/build-vsim.sh
+++ scripts/build-vsim.sh
@@ -2,7 +2,7 @@
 
 pushd vsim
 tar -czh . | docker build -t vsim -
-if [ -f /run/k3s/containerd/containerd.sock ]; then
+if [ -e /run/k3s/containerd/containerd.sock ]; then
 docker save vsim:latest | ctr -a /run/k3s/containerd/containerd.sock -n=k8s.io images import -
 fi
 popd
