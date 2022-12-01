9ed1f32961423a06965b5e89f628f982fed2a81a
Francesco Salamida <salamida@amazon.com>
2022-12-01 16:15:00 +0100

    updated deploy-cloud script for ubuntu

--- scripts/deploy-cloud.sh
+++ scripts/deploy-cloud.sh
@@ -2,8 +2,8 @@
 sudo apt-get -y install jq gettext bash-completion moreutils
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
-echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a /home/ec2-user/.bash_profile
-echo "export AWS_REGION=${AWS_REGION}" | tee -a /home/ec2-user/.bash_profile
+echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a /home/ubuntu/.bash_profile
+echo "export AWS_REGION=${AWS_REGION}" | tee -a /home/ubuntu/.bash_profile
 aws configure set default.region ${AWS_REGION}
 aws configure set default.account ${ACCOUNT_ID}
 git config --global core.autocrlf false
