b089bdc67889d5abedc329793789d8260115ed0a
rmncardoso <20070128+rmncardoso@users.noreply.github.com>
2024-04-17 22:44:44 +0100

    deps fix

--- cloud/.projenrc.py
+++ cloud/.projenrc.py
@@ -3,12 +3,12 @@ from projen.awscdk import AwsCdkPythonApp
 project = AwsCdkPythonApp(
     author_email="salamida@amazon.com",
     author_name="Francesco Salamida",
-    cdk_version="2.27.0",
+    cdk_version="2.13.0",
     module_name="src",
     name="demo-soafee-aws-iotfleetwise",
     version="0.1.0",
     deps=[
-        'cdk-aws-iotfleetwise==0.3.2',
+        'cdk-aws-iotfleetwise>=0.3.2',
     ]
 )
 
